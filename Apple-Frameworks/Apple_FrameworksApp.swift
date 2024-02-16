4cf6aea605cf2ea6a2737af486f7c4c18d8109b9
cjungwo <wjd516@gmail.com>
2024-02-17 04:11:38 +1100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by JungWoo Choi on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
